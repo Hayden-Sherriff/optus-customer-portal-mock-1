c5131d56bd816908cb809d1e6b81704dc919b902
Hayden-Sherriff <sherriff.h@gmail.com>
2026-03-31 14:27:41 +1100

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BillingComponent } from './billing/billing.component';
+import { AccountComponent } from './account/account.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    BillingComponent,
+    AccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
