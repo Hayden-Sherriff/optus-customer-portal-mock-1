e5dd725de43418bc4bfedb7f4e1c0d09d62d9bc0
Hayden-Sherriff <sherriff.h@gmail.com>
2026-03-31 14:33:55 +1100

    Create account.component.ts

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+})
+export class AccountComponent implements OnInit {
+  accountForm: FormGroup;
+
+
+  constructor(
+    private http: HttpClient,
+    private fb: FormBuilder
+  ) {}
+
+
+  ngOnInit(): void {
+    this.accountForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required]
+    });
+  }
+
+
+  onSubmit(): void {
+    if (this.accountForm.valid) {
+      this.http.put('/api/account', this.accountForm.value).subscribe();
+    }
+  }
+}
